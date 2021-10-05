7a8d5eaea04add5d8a26963562c50c11ce4d3b13
quentinb28 <quentin.b@investing.com>
2021-10-05 09:07:41 +0200

    added new directory minion-game

--- codility/max-non-overlapping-segments.py
+++ codility/max-non-overlapping-segments.py
@@ -1,5 +1,9 @@
 def solution(A, B):
 
+    #  A: starting indexes
+    #  B: ending indexes
+    #  Both need to be sorted by ending indexes in ascending order
+
     last_end_segment = -1
 
     chosen_count = 0
@@ -17,9 +21,8 @@ def solution(A, B):
 
 if __name__ == '__main__':
 
-    # Doubt with the case below, the code does not take that case into account - check answer in Q&A
-    # A, B = [1, 2, 3, 7, 9, 9], [5, 3, 6, 8, 9, 10]
+    A, B = [2, 4, 1, 7, 9, 9], [3, 5, 6, 8, 9, 10]
 
-    A, B = [1, 3, 7, 9, 9], [5, 6, 8, 9, 10]
+    # sorted(list(zip(A, B)), key=lambda x: x[1])
 
-    solution(A, B)
+    print(solution(A, B))
---
+++ minion-game/minion-game-exercise.py
@@ -0,0 +1,24 @@
+import re
+
+
+def minion_game(string):
+
+    vowels = ["A", "E", "I", "O", "U"]
+
+    Stuart = []  # Words (Consonants)
+    Stuart_score = 0
+
+
+
+    Kevin = []  # Words (Vowels)
+    Kevin_score = 0
+
+
+if __name__ == '__main__':
+    string = "BANANA"
+
+s = 'banana'
+
+vowels = ("A", "E", "I", "O", "U")
+
+print(re.findall("a[a-z]{1,*}", s))
