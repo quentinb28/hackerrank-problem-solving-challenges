66d718435f3041866b464f131467af6f0bcc046b
quentinb28 <bracq.quentin@gmail.com>
2021-10-10 18:24:53 +0200

    finished challenge

--- minion-game/minion-game-exercise.py
+++ minion-game/minion-game-exercise.py
@@ -1,24 +1,40 @@
-import re
+def minion_game(s):
+    """
+    Input is a string.
+    Both players attempt to make as many words as possible.
+    One with words starting with a vowel.
+    Another with words starting with consonants.
+    :param s:
+    :return:
+    """
+
+    # Function checks if letter is a vowel
+    def is_vowel(x):
+        vowels = ["A", "E", "I", "O", "U"]
+        if x in vowels:
+            return True
+        return False
+
+    kevin_counter = 0
+    stuart_counter = 0
+
+    # Adds all possibilities starting with current letter
+    for i in range(len(s)):
+        if is_vowel(s[i]):
+            kevin_counter += (len(s) - i)
+        else:
+            stuart_counter += (len(s) - i)
+
+    if kevin_counter > stuart_counter:
+        print("Kevin", kevin_counter)
+    elif stuart_counter > kevin_counter:
+        print("Stuart", stuart_counter)
+    else:
+        print("Draw")
 
 
-def minion_game(string):
+if __name__ == "__main__":
 
-    vowels = ["A", "E", "I", "O", "U"]
-
-    Stuart = []  # Words (Consonants)
-    Stuart_score = 0
-
-
-
-    Kevin = []  # Words (Vowels)
-    Kevin_score = 0
-
-
-if __name__ == '__main__':
     string = "BANANA"
 
-s = 'banana'
-
-vowels = ("A", "E", "I", "O", "U")
-
-print(re.findall("a[a-z]{1,*}", s))
+    minion_game(string)
